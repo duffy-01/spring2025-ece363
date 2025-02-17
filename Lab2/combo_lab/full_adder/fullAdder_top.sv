7a8dc6a575162f050da48a7a08abdfed4a6f344c
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-02-17 13:08:20 -0500

    Update fullAdder_top.sv

--- Lab2/combo_lab/full_adder/fullAdder_top.sv
+++ Lab2/combo_lab/full_adder/fullAdder_top.sv
@@ -3,9 +3,9 @@ module fullAdder_top;
 parameter sim_cycle=100;
 reg sys_clk;
 
-dual_io top_io(sys_clk);
-dual_test test(top_io);
-dual_pattern dut(.reset(top_io.reset), .clk(top_io.clk), .w(top_io.w), .z(top_io.z));
+fullAdder_if top_if(sys_clk);
+fullAdder_tb test(top_io);
+  dual_pattern dut(.a(top_io.a), .clk(top_io.clk), .b(top_io.b), .carryIn(top_io.carryIn), .carry(top_io.carry), .sum(top_io.sum));
 
 //clk
 initial
