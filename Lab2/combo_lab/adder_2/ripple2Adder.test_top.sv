a1fae1f5e0e0ec53ee2e95603dc2150ac4f2d13e
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-02-17 22:06:41 -0500

    Update and rename ripple2Adder_harness.sv to ripple2Adder.test_top.sv

---
+++ Lab2/combo_lab/adder_2/ripple2Adder.test_top.sv
@@ -0,0 +1,21 @@
+//*************************************************************************
+// Author(s): Shane Duffy
+// Creation Date: 10 February 2025
+// Class: ECE 363 - Design of Digital Systems
+// Lab this program is associated with: Lab 2
+// Lab due date: 17 February 2025
+//
+// Purpose of program: Harness for single full adder unit.
+//
+// Additional files needed: N/A
+//
+// Date of last modification: 17 February 2025 @ 17:48
+//*************************************************************************
+
+module rippleADD_top;
+
+rippleADD_io top_io();
+rippleADD DUT(.a(top_io.a), .b(top_io.b), .carryIn(top_io.carryIn), .carry(top_io.carry), .sum(top_io.sum));
+rippleADD_testbench(top_io);
+
+endmodule
